cb0b9e3c27f30998a6c75dce80e19cb1920cf2e8
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-04-13 23:01:01 -0400

    themeProvider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { useState } from "react";
 import './App.css';
+import {ThemeProvider} from "styled-components"
 
 function App() {
+  const {theme, setTheme} = useState("light")
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  }
+
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={theme ===light ? "" : ""}>
+       <div className="App"></div>
+    </ThemeProvider>
   );
 }
 
